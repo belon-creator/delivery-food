bf35f8c843f290044e6688d1f485dcb983c51920
belon-creator <belon8911@gmail.com>
2020-10-29 21:21:59 +0200

    add authorization

--- js/main.js
+++ js/main.js
@@ -1,10 +1,76 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
 
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  modal.classList.toggle("is-open");
+  modal.classList.toggle('is-open');
 }
+
+// ==================================
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+function toogleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+  console.log('authorized');
+
+  function logOut() {
+    login = null;
+    localStorage.removeItem('gloDelivery');
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+  }
+
+  buttonAuth.style.display = 'none';
+  userName.textContent = login;
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+  console.log('notAuthorized');
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+    localStorage.setItem('gloDelivery', login);
+    toogleModalAuth();
+    buttonAuth.removeEventListener('click', toogleModalAuth);
+    closeAuth.removeEventListener('click', toogleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener('click', toogleModalAuth);
+  closeAuth.addEventListener('click', toogleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
