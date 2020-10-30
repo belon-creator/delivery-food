94fe4650cfd34f17c72e9717425501e1c3585809
belon-creator <belon8911@gmail.com>
2020-10-30 21:08:02 +0200

    add more js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const cartButton = document.querySelector('#cart-button');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
@@ -18,6 +20,12 @@ const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
 
@@ -92,3 +100,87 @@ function checkAuth() {
 }
 
 checkAuth();
+
+function createCardRestaurant(params) {
+  const card = `
+  <a class="card card-restaurant">
+  <img
+    src="img/tanuki/preview.jpg"
+    alt="image"
+    class="card-image"
+  />
+  <div class="card-text">
+    <div class="card-heading">
+      <h3 class="card-title">Тануки</h3>
+      <span class="card-tag tag">60 мин</span>
+    </div>
+   
+    <div class="card-info">
+      <div class="rating">4.5</div>
+      <div class="price">От 1 200 грн</div>
+      <div class="category">Суши, роллы</div>
+    </div>    
+  </div>  
+</a>
+
+  `;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+createCardRestaurant();
+
+function createCardGood() {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+  <img
+  src="img/pizza-plus/pizza-classic.jpg"
+  alt="image"
+  class="card-image"
+/>
+<div class="card-text">
+  <div class="card-heading">
+    <h3 class="card-title card-title-reg">Пицца Классика</h3>
+  </div>
+  <!-- /.card-heading -->
+  <div class="card-info">
+    <div class="ingredients">
+      Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина,
+      салями, грибы.
+    </div>
+  </div>
+  <!-- /.card-info -->
+  <div class="card-buttons">
+    <button class="button button-primary button-add-cart">
+      <span class="button-card-text">В корзину</span>
+      <span class="button-cart-svg"></span>
+    </button>
+    <strong class="card-price-bold">510 грн</strong>
+  </div>
+  `;
+  cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {
+  const target = event.target;
+  const restaurant = target.closest('.cards-restaurants');
+
+  if (restaurant) {
+    cardsMenu.textContent = '';
+    containerPromo.classList.add('hide');
+    restaurants.classList.add('hide');
+    menu.classList.remove('hide');
+
+    createCardGood();
+    createCardGood();
+    createCardGood();
+  }
+}
+
+cardsRestaurants.addEventListener('click', openGoods);
+logo.addEventListener('click', function () {
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+});
